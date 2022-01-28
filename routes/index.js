d9cedba04d71db65f7ed4ca48b4812c150ff2670
deosaju <deosaju2260@gmail.com>
2022-01-28 09:36:07 +0530

    Pages

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {page:'Home', menuId:'home'});
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About Us', menuId:'about'});
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', {});
 });
+
+router.get('/charts', function(req, res, next) {
+  res.render('charts', {});
+});
+
+router.get('/news', function(req, res, next) {
+  res.render('news', {});
+});
+
 
 module.exports = router;
