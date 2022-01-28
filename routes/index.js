d6bb4846f80386d60db86254d97b95ebb200c3e8
Sidharth P <sidharthpunathil714@gmail.com>
2022-01-28 11:48:50 +0530

    it's working now... don't know if it's the best way to do! Probably.NOT!

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,48 @@
 var express = require("express");
+const res = require("express/lib/response");
 var router = express.Router();
 let resp;
 const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("409d03c2dbca429b8e447f38c64e45db");
 // To query /v2/top-headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2
-  .topHeadlines({
+
+const getNews = async () => { 
+
+  let result = newsapi.v2.topHeadlines({
     q: "bitcoin",
     category: "business",
     language: "en",
     country: "us",
   })
-  .then((response) => {
-    let articles = response.articles;
-    console.log(articles);
-    return articles;
+  
+  return await result
+}
+
+
+const main = async () => {
+  let data = await getNews()
+  let articles = data.articles;
+  console.log(articles);
+  //   return articles;
+
+    router.get("/", function (req, res, next) {
+      res.render("index", { page: "Home", menuId: "home"});
+    });
+
+    router.get("/dashboard", function (req, res, next) {
+      res.render("dashboard", {});
     });
     
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { page: "Home", menuId: "home" });
-});
+    router.get("/charts", function (req, res, next) {
+      res.render("charts", {});
+    });
+    
+    router.get("/news", function (req, res, next) {
+      res.render("news", {articles: "articles"});
+    });
 
-router.get("/dashboard", function (req, res, next) {
-  res.render("dashboard", {});
-});
+}
 
-router.get("/charts", function (req, res, next) {
-  res.render("charts", {});
-});
-
-router.get("/news", function (req, res, next) {
-  res.render("news", {});
-});
-
+main();
 module.exports = router;
